3d7ff11c94e5a0102dfae840830e2760cdab9140
pavsyd <58871761+pavsyd@users.noreply.github.com>
2022-09-27 19:51:34 +0300

    commit

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+# Configure the page
+st.set_page_config(
+   page_title="CM",
+   page_icon="📄",
+   layout="centered",
+   initial_sidebar_state="collapsed",
+)
+
